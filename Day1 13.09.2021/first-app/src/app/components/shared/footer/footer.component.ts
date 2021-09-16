1f24ea0ccf80338cf58752ad548c3671279c1171
Ashish Mishra <ashishmishra9426@gmail.com>
2021-09-16 11:54:40 +0530

    angular

---
+++ Day1 13.09.2021/first-app/src/app/components/shared/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "app-footer",
+    // template: `<div class="footer"><h1>I am footer</h1></div>`,
+    templateUrl:'./footer.component.html',
+    // styles:[".footer{height:120px; background:green}"],
+    styleUrls:['./footer.component.css'],
+})
+
+export class FooterComponent{
+
+}
